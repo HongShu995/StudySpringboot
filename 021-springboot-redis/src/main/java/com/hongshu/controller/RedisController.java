c5b11a093e716df5aa2aceaec6270a81e8717377
HongShu995 <759631477@qq.com>
2021-11-22 11:26:17 +0800

    第三天

--- 021-springboot-redis/src/main/java/com/hongshu/controller/RedisController.java
+++ 021-springboot-redis/src/main/java/com/hongshu/controller/RedisController.java
@@ -1,8 +1,11 @@
 package com.hongshu.controller;
 
+import com.hongshu.vo.Student;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +68,49 @@ public class RedisController
         // 使用StringRedisTemplate对象
         String v = stringRedisTemplate.opsForValue().get(k);
         return "K的Value: "+v;
+    }
+
+    /**设置RedisTemplate的序列化机制
+     * 可以设置 key 的序列化， 可以设置value的序列化
+     * 可以同时设置 key 和 value的序列化
+     */
+
+    @PostMapping("/redis/addstr")
+    public String addString(String k,String v)
+    {
+        //使用redisTemplate
+        //设置key的序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置value的序列化
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.opsForValue().set(k,v);
+        return "定义了RedisTemplate的key与value序列化";
+    }
+
+    /**
+     * 使用json 序列化， 把java对象转为json存储
+     */
+
+    @PostMapping("/redis/addjson")
+    public String addJson()
+    {
+        Student student = new Student();
+        student.setId(1001);
+        student.setName("zhangsan");
+        student.setAge(20);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(Student.class));
+        redisTemplate.opsForValue().set("mystudent",student);
+        return "json的序列化";
+    }
+
+    @GetMapping("/redis/getjson")
+    public String getJson()
+    {
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(Student.class));
+        Object obj = redisTemplate.opsForValue().get("mystudent");
+        return "json的反序列化="+obj;
     }
 }
